a0de50e3f16f935c5ebcba6a83019c88620144b4
Jarod Klypchak <31743632+JarodKlypchak@users.noreply.github.com>
2017-10-21 09:43:00 -0400

    f

--- scripts/testMovement.js
+++ scripts/testMovement.js
@@ -4,16 +4,11 @@ main();
 
 function main() {
     setUpCanvas();
-    //<<<<<<< HEAD
-    var person = Person(50, 50);
-    person.display();
-    let ground = Ground(50);
-    ground.display();
-    //=======
+
 
 
     var temp = setInterval(test, 10);
->>>>>>> 6daf9f10f387cf043efea5feef9c5a792efed8fb
+
 }
 
 function setUpCanvas() {
@@ -24,7 +19,7 @@ function setUpCanvas() {
 }
 
 function test() {
-    canvas.clearRec
+
     person.moveX(5);
     person.display();
 }
