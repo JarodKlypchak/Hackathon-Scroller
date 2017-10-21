adb928c439f0d4852248ace81ad758c8a4cb5ccc
Jarod Klypchak <31743632+JarodKlypchak@users.noreply.github.com>
2017-10-21 11:17:50 -0400

    f

--- scripts/testMovement.js
+++ scripts/testMovement.js
@@ -1,6 +1,7 @@
 let canvas = document.getElementById("c");
 let person = new Person(0, 250);
 main();
+let time = 0;
 
 function main() {
     setUpCanvas();
@@ -16,24 +17,38 @@ function setUpCanvas() {
     canvas.style.backgroundColor = "lightgray";
     canvas.style.border = "1px solid black";
 }
+let jumping = false;
 
 function test() {
-    document.body.onkeydown = function(e) {
-        checkKey(e);
+
+    if (jumping) {
+        time += 10;
+        if (time == 500) {
+            time = 0;
+            jumping = false;
+        }
+        jump(time);
     }
+
     setUpCanvas();
     person.display(canvas);
 }
 
-function checkKey(e) {
+function jump(time) {
+    if (time < 100) {
+        person.moveY(-10);
+    } else {
+        person.moveY(2);
+    }
+}
 
-
+document.body.onkeydown = function(e) {
     if (e.keyCode == "39") {
         person.moveX(5);
     } else if (e.keyCode == "37") {
         person.moveX(-5);
-    }
-    if (e.keyCode == "32") {
-        person.moveY(-5);
+    } else if (e.keyCode == "32") {
+        jump(time);
+        jumping = true;
     }
 }
