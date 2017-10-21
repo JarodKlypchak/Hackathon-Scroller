1a31f7c884a228928eb54eff85483e4ccde617ef
Jarod Klypchak <31743632+JarodKlypchak@users.noreply.github.com>
2017-10-21 15:41:22 -0400

    f

--- scripts/GroundTest.js
+++ scripts/GroundTest.js
@@ -31,48 +31,11 @@ function game(arrayMonsters, canvas, arrayHoles, arrayPlatforms) {
         arrayMonsters[i].update(arrayHoles, arrayPlatforms, arrayMonsters, canvas);
     }
     person.handleJump(arrayPlatforms);
+
+
     person.moveX(moveDistance);
+
     setUpCanvas(arrayMonsters, canvas, arrayHoles, arrayPlatforms);
-
-        let changed = false;
-        for (let i = 0; i < arrayPlatforms.length; i++) {
-            if (person.x <= arrayPlatforms[i].x + arrayPlatforms[i].width && person.x + 32 >= arrayPlatforms[i].x && person.y < arrayPlatforms[i].y) {
-                h = arrayPlatforms[i].y - charWidth;
-                changed = true;
-            }
-        }
-        if (jumping) {
-            for (let i = 0; i < arrayPlatforms.length; i++) {
-                if (person.x <= arrayPlatforms[i].x + arrayPlatforms[i].width && person.x + 32 >= arrayPlatforms[i].x && person.y > arrayPlatforms[i].y) {
-                    if (person.belowObject(arrayPlatforms[i].y)) {
-                        time = jumpDuration;
-
-                    }
-                }
-            }
-            time += 5;
-            jump(time);
-
-            if (person.onObject(h)) {
-                time = 0;
-                jumping = false;
-                person.y = h;
-            }
-        }
-        if (!changed) {
-            h = baseHeight;
-        }
-
-        console.log(h);
-        //Handles Falling when not jumping
-        if (person.y < h && !jumping) {
-            jumping = true;
-            time = 150;
-        }
-
-        person.moveX(moveDistance);
-
-        setUpCanvas(arrayMonsters, canvas, arrayHoles, arrayPlatforms);
 }
 
 /*
