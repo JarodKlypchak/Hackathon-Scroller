b2b25b9156eb1b7c49365e0574f433bf8b0150ee
NickHackman <nicholazwow@gmail.com>
2017-10-21 09:25:13 -0400

    ground?

---
+++ scripts/terrain.js
@@ -0,0 +1,11 @@
+function ground{
+    constructor(y){
+        this.y = 50;
+    }
+
+    function display(canvas){
+        let c = c.getContext("2d");
+        c.fillStyle = "green";
+        c.fillRect(0, 1000, 0, this.y);
+    }
+}
