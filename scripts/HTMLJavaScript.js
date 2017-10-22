3185a0ceabbe8993b519f8c64369dc539efebe98
Jarod Klypchak <31743632+JarodKlypchak@users.noreply.github.com>
2017-10-22 10:16:20 -0400

    f

--- scripts/HTMLJavaScript.js
+++ scripts/HTMLJavaScript.js
@@ -16,20 +16,8 @@ function animate() {
 /*
  * event listener for play button
  */
-<<<<<<< HEAD
-
-document.getElementById("play").addEventListener("click", function() {
-
-    console.log("display");
-
-    document.getElementById("c").style.display = "block";
-    document.getElementById("play").style.display = "none";
-    document.getElementById("movingToLevel").innerHTML = "";
-
-=======
 document.getElementById("play").addEventListener("click", function() {
     document.getElementById("c").style.display = "block";
     document.getElementById("play").style.display = "none";
     document.getElementById("movingToLevel").innerHTML = "";
->>>>>>> 06455b558799119ac3be42d1d9a88cbf6147a600
 });
