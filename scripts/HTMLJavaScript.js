5d6de41b0ddb5aee69e883a7cbe674a5475ba18d
Jarod Klypchak <31743632+JarodKlypchak@users.noreply.github.com>
2017-10-22 09:47:17 -0400

    f

--- scripts/HTMLJavaScript.js
+++ scripts/HTMLJavaScript.js
@@ -21,4 +21,19 @@ document.getElementById("play").addEventListener("click", function() {
     document.getElementById("c").style.display = "block";
     document.getElementById("play").style.display = "none";
     document.getElementById("movingToLevel").innerHTML = "";
+<<<<<<< HEAD
 });
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 3cf7a46... pushhh
+    document.getElementById("pause").style.display = "block";
+>>>>>>> parent of 3cf7a46... pushhh
+});
+=======
+});
+>>>>>>> 249713c620938886e1c7bc9b3ca0d92b5d8c27df
+>>>>>>> parent of b37dfdf... f
