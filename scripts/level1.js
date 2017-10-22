f5d47a81ed34b1db907f4ad642f1ff8a92a840ec
NickHackman <nicholazwow@gmail.com>
2017-10-21 21:10:49 -0400

    PUSHHHU

--- scripts/level1.js
+++ scripts/level1.js
@@ -20,27 +20,32 @@ function createLevel1(canvas){
     return level;
 }
 function addHoles(level, canvas) {
-    let level[0] = new Hole(canvas, 50, 50);
-    let level[1] = new Hole(canvas, 200, 50);
-    let level[2] = new Hole(canvas, 350, 50);
+    level[0].push(new Hole(canvas, 50, 50));
+    level[0].push(new Hole(canvas, 200, 50));
+    level[0].push(new Hole(canvas, 350, 50));
+    level[0].push(new Hole(canvas, 1000, 700));
 }
 function addPlatforms(level, canvas) {
-    let level[0] = new Platform(50, 300, 100);
-    let platformScreen12 = new Platform(200, 300, 100);
-    let platformScreen13 = new Platform(400, 300, 100);
-    let wallScreen11 = new Platform(600, 400, 20, -50);
-
+    level[1].push(new Platform(50, 300, 100));
+    level[1].push(new Platform(200, 300, 100));
+    level[1].push( new Platform(400, 300, 100));
+    level[1].push(new Platform(600, 400, 20, -50));
+    level[1].push(new Platform(canvas.width + 200, 350, 200));
+    level[1].push(new Platform(canvas.width + 300, 350, 200));
+    level[1].push(new Platform(canvas.width + 425, 250, 50));
+    level[1].push(new Platform(canvas.width + 425, 150, 50));
+    level[1].push(new Platform(canvas.width + 425, 50, 50));
 }
 function addMonsters(level, canvas) {
-    let level[0] = new Monster(800, 375);
-    let level[1] = new Monster(750, 375);
-    let level[2] = new Monster(50, 275);
-    let level[3] = new Monster(1105, 324);
-    let level[4] = new Monster(1160, 324);
-    let level[5] = new Monster(1200, 324);
-    let level[6] = new Monster(1405, 324);
-    let level[7] = new Monster(1460, 324);
-    let level[8] = new Monster(1500, 324);
+    level[2].push(new Monster(800, 375));
+    level[2].push(new Monster(750, 375));
+    level[2].push(new Monster(50, 275));
+    level[2].push(new Monster(1105, 324));
+    level[2].push(new Monster(1160, 324));
+    level[2].push(new Monster(1200, 324));
+    level[2].push(new Monster(1405, 324));
+    level[2].push(new Monster(1460, 324));
+    level[2].push(new Monster(1500, 324));
 }
 function addCoins() {
 
