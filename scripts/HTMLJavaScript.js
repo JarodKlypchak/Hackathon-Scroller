0cc9d374e06e7f87f0695ca67c2f178ca087cef9
Jarod Klypchak <31743632+JarodKlypchak@users.noreply.github.com>
2017-10-22 08:42:04 -0400

    f

--- scripts/HTMLJavaScript.js
+++ scripts/HTMLJavaScript.js
@@ -4,10 +4,10 @@
 let hammenu = document.getElementById("hamburgermenu");
 hammenu.addEventListener("click", animate);
 
-function animate(){
+function animate() {
     document.getElementById("navbar").classList.toggle("animate");
     let dropdown = document.getElementById("dropdown");
-    if(dropdown.style.top == "52px")
+    if (dropdown.style.top == "52px")
         dropdown.style.top = "-100%";
     else
         dropdown.style.top = "52px";
@@ -16,7 +16,7 @@ function animate(){
 /*
  * event listener for play button
  */
-document.getElementById("play").addEventListener("click", function(){
+document.getElementById("play").addEventListener("click", function() {
     document.getElementById("c").style.display = "block";
     document.getElementById("play").style.display = "none";
     document.getElementById("movingToLevel").innerHTML = "";
