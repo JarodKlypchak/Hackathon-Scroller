59c427f8c76e8acab1e7f9ddb963f379ef9036fe
NickHackman <nicholazwow@gmail.com>
2017-10-22 08:27:43 -0400

    pushing really fast

--- scripts/HTMLJavaScript.js
+++ scripts/HTMLJavaScript.js
@@ -20,4 +20,5 @@ document.getElementById("play").addEventListener("click", function(){
     document.getElementById("c").style.display = "block";
     document.getElementById("play").style.display = "none";
     document.getElementById("movingToLevel").innerHTML = "";
+    document.getElementById("pause").style.display = "block";
 });
