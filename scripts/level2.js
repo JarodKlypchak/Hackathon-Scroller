8caffb9bc0434617fd48224389c2fb37d9fe236e
thstein7 <31679672+thstein7@users.noreply.github.com>
2017-10-21 20:52:07 -0400

    a

--- scripts/level2.js
+++ scripts/level2.js
@@ -24,13 +24,28 @@ function addHoles(level, canvas) {
     level[0].push(hole);
     let hole2 = new Hole(canvas, 200, 200);
     level[0].push(hole2);
+    let hole3 = new Hole(canvas, 600, 100);
+    level[0].push(hole3);
+    let hole4 = new Hole(canvas, canvas.width, 50);
+    level[0].push(hole4);
 }
-function addPlatforms(level, canvas) {
-
+function addPlatforms(level) {
+    let platform = new Platform(250, 300, 100);
+    level[1].push(platform);
 }
-function addMonsters(level, canvas) {
-
+function addMonsters(level) {
+    let monster = new Monster(500,375);
+    level[2].push(monster);
+    let monster2 = new Monster(175, 375);
+    level[2].push(monster2);
+    let monster3 = new Monster(75,375);
+    level[2].push(monster3);
+    //let monster4 = new Monster(canvas.width+200, 375);
+    //level[2].push(monster4);
 }
-function addCoins() {
-
+function addCoins(level) {
+    let coin = new Coin(650,250);
+    level[3].push(coin);
+    let coin2 = new Coin(300,150);
+    level[3].push(coin2);
 }
