3fa94b9133ec1d6ba3186e71755be61e52bfb911
Jarod Klypchak <31743632+JarodKlypchak@users.noreply.github.com>
2017-10-22 09:40:04 -0400

    f

--- scripts/Levels/level3.js
+++ scripts/Levels/level3.js
@@ -25,6 +25,17 @@ function addHoles3(level, canvas) {
     level[0].push(hole);
     let hole2 = new Hole(canvas, 450, 160);
     level[0].push(hole2);
+    let hole3 = new Hole(canvas, canvas.width + 450, 200);
+    level[0].push(hole3);
+
+    hole3 = new Hole(canvas, canvas.width * 2 + 100, 150);
+    level[0].push(hole3);
+    hole3 = new Hole(canvas, canvas.width * 2 + 300, 150);
+    level[0].push(hole3);
+    hole3 = new Hole(canvas, canvas.width * 2 + 500, 150);
+    level[0].push(hole3);
+    hole3 = new Hole(canvas, canvas.width * 2 + 700, 150);
+    level[0].push(hole3);
 }
 
 function addPlatforms3(level, canvas) {
@@ -32,11 +43,23 @@ function addPlatforms3(level, canvas) {
     level[1].push(platform);
     let platform2 = new Platform(canvas.width + 150, 300, 300);
     level[1].push(platform2);
+    platform2 = new Platform(canvas.width + 100, 100, 300);
+    level[1].push(platform2);
+    platform2 = new Platform(canvas.width + 75, 200, 100);
+    level[1].push(platform2);
+    let platform3 = new Platform(canvas.width + 650, 400, 100, -200);
+    level[1].push(platform3);
 }
 
 function addMonsters3(level, canvas) {
     let monster = new Monster(800, 375);
     level[2].push(monster);
+
+    //screen Monsters
+    monsterw = new Monster(canvas.width + 170, 275);
+    level[2].push(monsterw);
+    monsterw = new Monster(canvas.width + 70, 375);
+    level[2].push(monsterw);
 
 }
 
