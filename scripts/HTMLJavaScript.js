cd766ad538ced183f7f605ce15c431fc717b4b3f
JarodKlypchak <31743632+JarodKlypchak@users.noreply.github.com>
2017-10-24 00:10:29 -0400

    f

--- scripts/HTMLJavaScript.js
+++ scripts/HTMLJavaScript.js
@@ -28,37 +28,37 @@ document.getElementById("play").addEventListener("click", function() {
 /*
  * event listener for left button pressed down
  */
-document.getElementById("left").addEventListener("mousedown", function(event){
+document.getElementById("left").addEventListener("mousedown", function(event) {
     person.movingLeft = true;
 });
 
 /*
  * event listener for left button not down
  */
-document.getElementById("left").addEventListener("mouseup", function(event){
+document.getElementById("left").addEventListener("mouseup", function(event) {
     person.movingLeft = false;
 });
 
 /*
  * event listener for right button pressed down
  */
-document.getElementById("right").addEventListener("mousedown", function(event){
+document.getElementById("right").addEventListener("mousedown", function(event) {
     person.movingRight = true;
 });
 
 /*
  * event listener for right button not pressed
  */
-document.getElementById("right").addEventListener("mouseup", function(event){
+document.getElementById("right").addEventListener("mouseup", function(event) {
     person.movingRight = false;
 });
 
 /*
  * event listener for jump button
  */
-document.getElementById("jump").addEventListener("click", function(event){
-    if(!person.jumping){
-        person.velocity = 10;
+document.getElementById("jump").addEventListener("click", function(event) {
+    if (!person.jumping) {
+        person.velocity = person.jumpVelocity;
         person.jumping = true;
     }
 });
